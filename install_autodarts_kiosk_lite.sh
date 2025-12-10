f067445598c97be94ff5ec47a1fb7932de746abb
Johannes-Offenburger <jojo.offenburger@web.de>
2025-12-10 17:07:09 +0100

    create repo

---
+++ install_autodarts_kiosk_lite.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "Autodarts Kiosk Installer gestartet..."
+
+# Hier kommen deine Install-Befehle hin, z.B.:
+# sudo apt update
+# sudo apt install -y chromium xdotool unclutter curl xbindkeys
